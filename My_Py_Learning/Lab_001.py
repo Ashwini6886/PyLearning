69116885ca69ef09e58ed004d9e85597acccec47
ashwini <ashwinitk66@gmail.com>
2024-07-11 22:02:55 +0530

    adding the Lab_001 file

---
+++ My_Py_Learning/Lab_001.py
@@ -0,0 +1,9 @@
+#source code which we are going to add
+print("Hello World")
+#control D it will be duplicated
+print("Hello World")
+#supports mathematics operations also
+print(2*2)
+print(2+2)
+print(2-1)
+print(2/2)
